e6bf262d5b9204718fb1743dd8d379533658a0fd
Data Monev LKPP <data1.monev@gmail.com>
2022-11-24 14:03:58 +0700

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("failed to load .env file")
+	}
+	// other code
+}
